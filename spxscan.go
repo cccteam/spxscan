26148b6f9efc616c560dc17d4ce8460cc3af0051
Joseph Watson <jtwatson@linux-consulting.us>
2024-07-03 21:15:29 -0400

    feature: Add support for returning data with THEN RETURN clause

--- spxscan.go
+++ spxscan.go
@@ -3,6 +3,8 @@ package spxscan
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"cloud.google.com/go/spanner"
 )
@@ -21,4 +23,42 @@ func Get(ctx context.Context, db Querier, dst any, statement spanner.Statement) 
 // See API.Select for details.
 func Select(ctx context.Context, db Querier, dst any, statement spanner.Statement) error {
 	return DefaultAPI.Select(ctx, db, dst, statement)
+}
+
+type TxnRunner interface {
+	ReadWriteTransaction(ctx context.Context, f func(context.Context, *spanner.ReadWriteTransaction) error) (commitTimestamp time.Time, err error)
+}
+
+// UpdateAndGet is a package-level helper function that uses the DefaultAPI.Get() call inside a transaction.
+// This should be used when data is being returned after an update using the THEN RETURN clause.
+func UpdateAndGet(ctx context.Context, client TxnRunner, dst any, statement spanner.Statement) error {
+	_, err := client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		if err := DefaultAPI.Get(ctx, txn, dst, statement); err != nil {
+			return fmt.Errorf("spxscan.Get(): %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("ReadWriteTransaction(): %w", err)
+	}
+
+	return nil
+}
+
+// UpdateAndSelect is a package-level helper function that uses the DefaultAPI.Select() call inside a transaction.
+// This should be used when data is being returned after an update using the THEN RETURN clause.
+func UpdateAndSelect(ctx context.Context, client TxnRunner, dst any, statement spanner.Statement) error {
+	_, err := client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		if err := DefaultAPI.Select(ctx, txn, dst, statement); err != nil {
+			return fmt.Errorf("spxscan.Get(): %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("ReadWriteTransaction(): %w", err)
+	}
+
+	return nil
 }
