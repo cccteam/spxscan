1c5f227566bab267ba2d9ae6b262602d15f68e5a
Joseph Watson <joseph@cloud-team.com>
2024-07-11 23:27:24 +0000

    refactor: Move implementation to API

--- api.go
+++ api.go
@@ -70,6 +70,40 @@ func (api *API) Select(ctx context.Context, db Querier, dst any, statement spann
 	return nil
 }
 
+// UpdateAndGet is a package-level helper function that uses the API.Get() call inside a transaction.
+// This should be used when data is being returned after an update using the THEN RETURN clause.
+func (api *API) UpdateAndGet(ctx context.Context, client TxnRunner, dst any, statement spanner.Statement) error {
+	_, err := client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		if err := api.Get(ctx, txn, dst, statement); err != nil {
+			return fmt.Errorf("API.Get(): %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("ReadWriteTransaction(): %w", err)
+	}
+
+	return nil
+}
+
+// UpdateAndSelect is a high-level helper function that uses the API.Select() call inside a transaction.
+// This should be used when data is being returned after an update using the THEN RETURN clause.
+func (api *API) UpdateAndSelect(ctx context.Context, client TxnRunner, dst any, statement spanner.Statement) error {
+	_, err := client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		if err := api.Select(ctx, txn, dst, statement); err != nil {
+			return fmt.Errorf("API.Select(): %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("ReadWriteTransaction(): %w", err)
+	}
+
+	return nil
+}
+
 // ScanOne makes sure that there was exactly one row otherwise it returns an error.
 // Use NotFound function to check if there were no rows. After iterating ScanOne
 // closes the interator, and propagates any errors that could pop up.
